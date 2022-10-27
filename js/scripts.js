f162a47c255b3825f2c55ff95d47c45d4b7293a2
LukeCallahan <lukemcal@gmail.com>
2022-10-27 10:54:14 -0700

    connect html form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 //convert string to an array of strings
 
 function ccInputToArray (number){
-  const stringArray = number.split("");
+  let stringArray = number.split("");
+  for (let i=0; i < stringArray.length; i += 1){
+    stringArray[i] = parseInt(stringArray[i]);
+    for (let j=1; j <stringArray.length; j +=2){
+      stringArray[j] = stringArray[j] * 2;
+    }
+  }
   return stringArray;
 }
 
-ccInputToArray("443434545333344");
+window.addEventListener("load", function(){
+  let form = document.querySelector("form");
+  let result = document.getElementById("results");
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const number = document.getElementById("text-input1").value;
+    console.log(number);
+    let p = document.createElement("p");
+    let div = document.querySelector("div");
+    div.append(number);
+    p.append(number);
 
-
-
+    
+  });
+});
 //
